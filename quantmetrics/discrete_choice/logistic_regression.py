03ef35fecebc02256c6ca3b30dad02c1fbf02770
tomohirofukui <echizenn_brave8150@icloud.com>
2019-12-01 12:05:21 +0900

    succeed to load data

--- quantmetrics/discrete_choice/logistic_regression.py
+++ quantmetrics/discrete_choice/logistic_regression.py
@@ -1,8 +1,18 @@
-def logistic():
-    print('Hallo World!')
+import pandas as pd
+
+def optimize():
+    return
+
+def logistic(X, y):
+    """
+    X: explanatory variables(pandas.Dateframe)
+    y: dependent variables(pandas.Series)
+    """
+    print(X.head())
+    print(y.head())
     return 
 
 def cmmixlogit():
     "Mixed logit regression"
     print('Hallo World!')
-    return 
+    return
